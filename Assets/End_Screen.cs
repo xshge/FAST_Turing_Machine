b38d0034a1b69a705582e4c9d50f74f808522c4a
xshenge <xshen12138@outlook.com>
2025-11-30 00:40:29 -0500

    import video for end screen

--- Assets/End_Screen.cs
+++ Assets/End_Screen.cs
@@ -1,13 +1,34 @@
+using FAST;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class End_Screen : ActivityScreen
 {
+    [Header("Video")]
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+    [SerializeField]
+    private VideoPlayerFromFile videoPlayerFromFile;
+    [SerializeField]
+    private RawImage videoImage;
     override protected IEnumerator PlayScreenAnimation()
     {
-        Debug.Log("ending screen");
-        yield break;
+        // Load and initialize
+        videoImage.CrossFadeAlpha(0f, 0f, false);
+
+        videoPlayerFromFile.baseFileName = string.Format("Closing-Video.mp4", screenManager.teaserName);
+        videoPlayerFromFile.Load(FAST.Application.language);
+        videoPlayer.Prepare();
+        yield return new WaitUntil(() => videoPlayer.isPrepared);
+        videoPlayer.Play();
+        yield return new WaitForSecondsRealtime(0.1f);
+        videoImage.CrossFadeAlpha(1f, 0.5f, false);
+
+        yield return new WaitWhile(() => videoPlayer.isPlaying);
+       
     }
 
 
